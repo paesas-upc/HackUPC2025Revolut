03a734056215271559f03f52fc3e7eaa990b39f7
ANDREU LÓPEZ PÉREZ <andreu.lopez.perez@estudiantat.upc.edu>
2025-05-03 13:14:41 +0200

    Update geocluster.py

--- src/geocluster.py
+++ src/geocluster.py
@@ -1,1 +1,11 @@
+# src/geocluster.py
+
+import hdbscan
+import pandas as pd
+
+def cluster_locations(df, min_cluster_size=5):
+    coords = df[['latitude', 'longitude']].drop_duplicates().values
+    clusterer = hdbscan.HDBSCAN(min_cluster_size=min_cluster_size, metric='haversine')
+    df['cluster'] = clusterer.fit_predict(pd.DataFrame(coords))
+    return df
 
